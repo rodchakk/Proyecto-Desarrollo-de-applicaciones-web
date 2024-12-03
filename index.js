c85e57677200fe4d3073833dbfd6b3eed6361b58
Rodolfo Chacon <rudychacon01@gmail.com>
2024-12-02 22:01:28 -0600

    codigo funcionando

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
+const port = 3000;
 
 
 const app = express ();
 
 
+app.use (express.json ());
 
 
-app.post("/login", (req, res) => {
+
+
+
+
+//middleware
+const verificarToken = (req, res, next) => {
+    const authHeader = req.headers['authorization'];
+    
+    if (!authHeader) {
+        return res.status(401).json({message: 'token no proporcionado'});
+    }
+    
+    
+    next();
+    
+    }
+    
+
+
+
+
+
+
+
+app.post("/login", verificarToken, (req, res) => {
   res.send("usuario");
 
 });
@@ -21,18 +47,6 @@ app.post("/login", (req, res) => {
 
 
 
-//middleware
-const verificarToken = (req, res, next) => {
-const authHeader = req.headers['authorization'];
-
-if (!authHeader) {
-    return res.status(401).json({message: 'token no proporcionado'});
-}
-
-
-next();
-
-}
 
 
 
@@ -44,8 +58,7 @@ next();
 
 
 
-
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log("Servidor corriendo en http://localhost:3000");
   });
   
